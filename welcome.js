55dae190b88eff5893919508fca055950e03a4aa
unknown <kavish.agnihotri@iiitb.org>
2020-08-03 00:58:48 +0530

    Front End Updated

--- welcome.js
+++ welcome.js
@@ -1,27 +1,8 @@
-fetch("https://jsonplaceholder.typicode.com/users") 
-    
-    // Converting received data to JSON 
-    .then(response => response.json()) 
-    .then(json => { 
-   
-        // Create a variable to store HTML 
-        let li = `<tr><th>Name</th><th>Email</th></tr>`; 
-        
-        // Loop through each data and add a table row 
-        json.forEach(user => { 
-            li += `<tr> 
-                <td>${user.name} </td> 
-                <td>${user.email}</td>          
-            </tr>`; 
-        }); 
-   
-    // Display result 
-    document.getElementById("users").innerHTML = li; 
-}); 
+  var A=25.0961;
+  var B=85.3131;
 
 
-
-var mymap = L.map('mapid').setView([25.5941, 85.1376], 13);
+var mymap = L.map('mapid').setView([A,B],5);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F2aXNoMTIzNDUiLCJhIjoiY2s1dXdpYzE5MHh3eTNucnppaWdzcnN6diJ9.P_dWAsA1odFQMz8M3uTmNA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -32,13 +13,78 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1Ijoia2F2aXNoMTIzNDUiLCJhIjoiY2s1dXdpYzE5MHh3eTNucnppaWdzcnN6diJ9.P_dWAsA1odFQMz8M3uTmNA'
 }).addTo(mymap);
 
-var api = "webapi/domain";
-$.get(api, function (domain, status) {
-    if (status == "success") {
-        var domainOptions = "";
-        for (var i = 0; i < domain.length; i++) {
-            domainOptions += '<option value="' + domain[i].id + '">' + domain[i].discipline + ' ' + domain[i].branch + '</option>';
-        }
-        $("#domainId").append(domainOptions);
-    }
-});
+  var i=new XMLHttpRequest();
+  var e = document.getElementById("district");
+  var str = e.options[e.selectedIndex].value;
+      
+      i.onreadystatechange=function()
+      {
+        
+        if(i.readyState==4 )
+        {
+          myObj = JSON.parse(this.responseText); 
+      
+               A=myObj.features[0].geometry.coordinates[0];
+               B=myObj.features[0].geometry.coordinates[1];
+               console.log(A);
+               console.log(B);
+
+
+                var marker = L.marker([B, A]).addTo(mymap);
+
+                marker.bindPopup("My Location").openPopup();
+               // mymap = L.map('mapid').setView([A,B], 13);
+        }  
+        
+      };
+      
+      i.open("GET","http://api.mapbox.com/geocoding/v5/mapbox.places/"+str+".json?access_token=pk.eyJ1Ijoia2F2aXNoMTIzNDUiLCJhIjoiY2s1dXdpYzE5MHh3eTNucnppaWdzcnN6diJ9.P_dWAsA1odFQMz8M3uTmNA" ,true);
+      i.send();
+
+// var api = "webapi/domain";
+// $.get(api, function (domain, status) {
+//     if (status == "success") {
+//         var domainOptions = "";
+//         for (var i = 0; i < domain.length; i++) {
+//             domainOptions += '<option value="' + domain[i].id + '">' + domain[i].discipline + ' ' + domain[i].branch + '</option>';
+//         }
+//         $("#domainId").append(domainOptions);
+//     }
+// });
+
+  var j=new XMLHttpRequest();
+   var yield=2.3;
+      
+      j.onreadystatechange=function()
+      {
+        
+        if(j.readyState==4 )
+        {
+          myObj = JSON.parse(this.responseText); 
+      
+          console.log(myObj.id);
+          var data=myObj.id;
+          document.getElementById("yield").innerHTML="Yield ="+yield;
+          document.getElementById("avgyield").innerHTML="Avg Yield ="+data;
+        }  
+        
+      };
+      
+      j.open("GET","https://jsonplaceholder.typicode.com/todos/1" ,true);
+      j.send();
+
+
+      // Image Select  Bad =1 , Good = 2 , 
+     
+      var result_name="positive.png";
+
+      if(yield>=1 && yield<=2)
+        result_name="neutral.png";
+      else if(yield>2 && yield<=4)
+        result_name="positive.png";
+      else
+        result_name="negative.png";
+
+      document.getElementById('image').src=result_name;
+      var result_value=0;
+
--- welcome.php
+++ welcome.php
@@ -19,115 +19,126 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
    crossorigin=""/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <!-- Make sure you put this AFTER Leaflet's CSS -->
  <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
    crossorigin=""></script>
-
+   
+   
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Deep Crop Yield Prediction</h1>
+  
+    <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php"><b>DEEP CROP YIELD PREDICTION</b></a>
     </div>
-    
-
-    <div class="container-fluid">
-      <div class="row flex-xl-nowrap">
-        <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
-	<form name = "form1" action="modified.php" method = "post">    
-            <div class = "container">    
-                <div class = "form-group row">    
-                    <label>Crop Name:</label>    
-                    <input type = "text" name = "crop" value = "" required/>    
-                </div>    
-                <div class = "form_group">    
-                    <label>Area:</label>    
-                    <input type = "number" name = "area" value = "" required />    
-                </div>    
-                <div class = "form_group">    
-                    <label>Season:</label>    
-                    <select id="state" name="state">
-                        <option value="rabi">Rabi</option>
-                        <option value="kharif">Kharif</option>
-                    </select>     
-                </div> 
-		<div class = "form_group">    
-                    <label>State:</label>    
-                      <select id="state" name="state">
-                        <option value="Bihar">Bihar</option>
-                      </select> 
-                <div class = "form_group">    
-                    <label>District:</label>    
-                    <input type = "text" name = "district" value = "" required/>    
-                </div> 
-                <div class = "form_group">    
-                    <label>Pincode:</label>    
-                     <input type = "number" name = "area" value = "" required />  
-                </div> 
-
-                 <a href="" class="btn">Submit</a>
-
-                 <div class=row>
-                    <div id="mapid"></div>
-                    <div> 
-
-		      
-            </div>    
-        </form> 
-        </div>
-    </div>
-</div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="welcome.php"><b>HOME</b></a></li>
+      <li><a href="reset-password.php"><b>CHANGE PASSWORD?</b></a></li>
+      <li ><a href="#"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <b>Hi, <?php echo htmlspecialchars($_SESSION["username"]); ?></b>
+      </a></li>
       
-        <script type="text/javascript" src="welcome.js"></script> 
-<!-- 
-        <div class="container">
-    <div class="auth-form-body"> -->
+      <li><a class="dd" href="logout.php">
+    <!--   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
+   <b> Sign Out</b></a></li>
+    </ul></b>
+    <!-- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
+    
+  </div>
+   <img src="main_picture.jpeg" alt="Image missing">
+    </nav>
+  <div class="page-header">
+        <h2>Welcome to Deep Crop Yield Prediction</h2>
+    </div>
+    <br><br>
 
-      <!--   <form class="form-signin" id="admission-form" action="webapi/student" method="post"
-              enctype="multipart/form-data">
+    <h3><b>Input</b></h3><br>
 
-            <div class="form-label-group">
-                <input type="text" id="firstName" name="firstName" class="form-control"
-                       placeholder="First Name" required
+  <form name = "form1" action=".predict" method = "post">    
+            <div class = "container">    
+                <div class="row">
+
+                <div class="form-group col-md-4">
+                <input type="text" id="cropName" name="cropName" class="form-control "
+                       placeholder="Crop Name" required
                        autofocus>
-                <label for="firstName">First Name</label>
-            </div>
+                </div>
 
-            <div class="form-label-group">
-                <input type="text" id="middleName" name="middleName" class="form-control"
-                       placeholder="Middle Name">
-                <label for="middleName">Middle Name</label>
-            </div>
+                <div class="form-group col-md-4">
+                <input type="number" id="area" name="area" class="form-control "
+                       placeholder="Area" required
+                       autofocus>
+                <!-- <label for="area">Area</label> -->
+                </div>
 
-            <div class="form-label-group">
-                <input type="text" id="lastName" name="lastName" class="form-control"
-                       placeholder="Last Name">
-                <label for="lastName">Last Name</label>
-            </div>
-
-            <div class="form-label-group">
-                <input type="email" id="emailId" name="emailId" class="form-control"
-                       placeholder="Email Address">
-                <label for="emailId">Email Address</label>
-            </div>
-
-            <div class="form-label-group">
-                <select id="domainId" name="domainId" class="form-control">
-                    <option value="0" selected disabled>Select Domain</option>
+                <div class="form-group col-md-4">
+                <select id="season" name="season" class="form-control">
+                    <option value="rabi" selected >Rabi</option>
+                    <option value="kharif">Kharif</option>
                 </select>
-            </div>
+                </div>
 
-         
-            <br>
+                <div class="form-group col-md-4">
+                <select id="state" name="state" class="form-control">
+                    <option value="bihar" selected >Bihar</option>
+                </select>
+                </div>
 
-            <button class="btn btn-primary btn-block mt-4" type="submit">Register</button>
+                <div class="form-group col-md-4">
+                <select id="district" name="district" class="form-control">
+                    <option value="patna" selected >Patna</option>
+                    <option value="begusarai">Begusarai</option>
+                    <option value="darbhanga">Darbhanga</option>
+                </select>
+                </div>
+
+                <div class="form-group col-md-4">
+                <input type="number" id="pincode" name="pincode" class="form-control "
+                       placeholder="Pincode" required
+                       autofocus>
+                </div>
+
+                </div>
+                
+                 <button class="btn btn-primary  mt-4 " style="font-size:1.5vw" type="submit">Predict</button>
+
+                <br><br><br><br>
+
+                <div id="mapid"></div><br/><br/>
+                <div class="row">
+                        <h2><b>Crop Yield Prediction Results</b></h2></div><br/><br/>
+                            
+                          <div class="col-md-3"></div>
+        
+                         <div class="col-md-3">
+                              <img class="row" id="image" src="negative.png" style="width:220px;height:200px;"" alt="Click Submit">
+                         </div>
+
+                         <div class="col-md-3">
+                          <h2 id="yield"></h2>
+                          <h2 id="avgyield"></h2>
+                        </div>
+
+                        <div class="col-md-3"></div>
+                        
+                </div>
+                <!-- </div> -->
+ 
+               <!--  <p>
+                        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+                        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+                </p>   -->  
+        </form> 
+       
+        <script type="text/javascript" src="welcome.js"></script> 
+        
 
 
-        </form> -->
-    <!-- </div>
-</div> -->
 </body>
 </html>
